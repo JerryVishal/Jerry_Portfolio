5721b6fdaef273ddc195f489a15e40168f9fc21b
Jerryvishal26 <Jerryvishal26@users.noreply.github.com>
2025-01-03 11:03:08 -0500

    stairs added

--- my-portfolio/components/ui/StairTransition.jsx
+++ my-portfolio/components/ui/StairTransition.jsx
@@ -2,7 +2,7 @@
 
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
-import Stairs from "./stairs";
+import Stairs from "./Stairs";
 
 const StairTransition = () => {
   const pathname = usePathname();
